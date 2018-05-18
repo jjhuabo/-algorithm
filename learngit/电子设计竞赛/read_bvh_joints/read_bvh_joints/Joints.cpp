087d9aa76bc7a7ad7dac71f202f68b39476a1bbc
jjhuabo <baijiyao@126.com>
2018-05-18 21:18:25 +0800

    关节类body类设计完成

--- learngit/电子设计竞赛/read_bvh_joints/read_bvh_joints/Joints.cpp
+++ learngit/电子设计竞赛/read_bvh_joints/read_bvh_joints/Joints.cpp
@@ -1,14 +1,18 @@
 #include "stdafx.h"
 #include "Joints.h"
+#include<sstream>
 
 
 Joints::Joints()
 {
+
 }
 
 
 Joints::~Joints()
 {
+	this->_strvec.clear();
+	this->_jointmotiondatas.clear();
 }
 
 Joints::Joints(string filename)
@@ -22,20 +26,24 @@ Joints::Joints(string filename)
 	}
 	else
 	{
-
+		string result;
+		stringstream input(_strvec[_motiondatalinenumb]);
+		string data;
+		while (!out.eof())
+		{
+			getline(out, data, '\n');
+			_strvec.push_back(data);
+		}
 	}
 }
 
 Joints::Joints(string filename,string joint_name)
 {
+	this->_frame = 0;
 	this->filename = filename;
 	fstream out;
 	string data;                                 //每一行读取得数据
-	string _frame = "Frame Time";
 	out.open(filename, ios::in);
-	const char *p = nullptr;
-	getline(out, data, '\n');
-	p = strstr(data.c_str(), _frame.c_str());
 
 	if (out.fail())
 	{
@@ -46,23 +54,48 @@ Joints::Joints(string filename,string joint_name)
 		read_joint_name_lines(joint_name);
 		while (!out.eof())
 		{
-			if (p != NULL)
-			{
-				
-			}
+
+			getline(out, data, '\n');
+			_strvec.push_back(data);
 		}
 	}
+
+	out.close();
+	read_data_start_lines(filename);
+	string result;
+	stringstream input(_strvec[_motiondatalinenumb+_frame]);
+
+	while (input >> result)
+	{
+		_jointmotiondatas.push_back(result);
+	}
+
+	
+		//for (int i = 0; i < _jointmotiondatas.size(); i++)
+		//{
+		//	cout << _jointmotiondatas[i];
+		//}
+		//cout << endl;
+		//cout << _jointnumb << endl;
+		//cout << (_jointnumb - 1) * 6 + 3 << endl;
+		//cout << _motiondatalinenumb;
+
+	Xrotation = stringTodouble(_jointmotiondatas[(_jointnumb - 1) * 6 + 3]);
+	Yrotation = stringTodouble(_jointmotiondatas[(_jointnumb - 1) * 6 + 4]);
+	Zrotation = stringTodouble(_jointmotiondatas[(_jointnumb - 1) * 6 + 5]);
 }
 
+//判断是第几个关节，便于后面从数据区取出相应得旋转角
 void Joints::read_joint_name_lines(string joint_name) 
 {
+	_jointnumb = 0;
 	fstream out;
 	string data;
-	const char *point;
-	const char *pointtojoint; //指向joints关键字
+	const char *point;        //指向joint_name关节
+	const char *pointtojoint; //指向Joint关键字
 	string joint = "JOINT";
-
 	out.open(filename, ios::in);
+
 	while (!out.eof())
 	{
 		getline(out, data, '\n');
@@ -79,5 +112,92 @@ void Joints::read_joint_name_lines(string joint_name)
 			}
 		}
 	}
+}
 
+//读取数据所在得行号
+void Joints::read_data_start_lines(string filename)
+{
+	string _frame = "Frame Time";
+	string data;
+	const char *p = nullptr;
+	fstream out;
+	out.open(filename, ios::in);
+
+	while (!out.eof())
+	{
+		getline(out, data, '\n');
+		p = strstr(data.c_str(), _frame.c_str());
+		_motiondatalinenumb++;
+		if (p != NULL)
+		{
+			//_motiondatalinenumb ++;
+			break;
+		}
+	}
+
+	out.close();
+}
+
+//自动读取joint关节得旋转角
+double Joints::readXroation(string joint)
+{
+	_jointmotiondatas.clear();
+	read_joint_name_lines(joint);
+
+	string result;
+	stringstream input(_strvec[_motiondatalinenumb+_frame]);
+
+	while (input >> result)
+	{
+		_jointmotiondatas.push_back(result);
+	}
+
+	this->Xrotation = stringTodouble(_jointmotiondatas[(_jointnumb - 1) * 6 + 3]);
+	return Xrotation;
+}
+
+//自动读取joint关节得Y轴旋转值
+double Joints::readYroation(string joint)
+{
+	_jointmotiondatas.clear();
+	read_joint_name_lines(joint);
+	string result;
+	stringstream input(_strvec[_motiondatalinenumb+_frame]);
+
+	while (input >> result)
+	{
+		_jointmotiondatas.push_back(result);
+	}
+
+	this->Yrotation = stringTodouble(_jointmotiondatas[(_jointnumb - 1) * 6 + 4]);
+	return Yrotation;
+}
+
+//自动读取joint关节得Z轴旋转值
+double Joints::readZroation(string joint)
+{
+	_jointmotiondatas.clear();
+	read_joint_name_lines(joint);
+	string result;
+	stringstream input(_strvec[_motiondatalinenumb+_frame]);
+
+	while (input >> result)
+	{
+		_jointmotiondatas.push_back(result);
+	}
+
+	this->Zrotation = stringTodouble(_jointmotiondatas[(_jointnumb - 1) * 6 + 5]);
+	return Zrotation;
+}
+
+void Joints::movetonextframe()
+{
+	_frame++;
+}
+
+
+double Joints::stringTodouble(string str)
+{
+	double a = atof(str.c_str());
+	return a;
 }
--- learngit/电子设计竞赛/read_bvh_joints/read_bvh_joints/Joints.h
+++ learngit/电子设计竞赛/read_bvh_joints/read_bvh_joints/Joints.h
@@ -1,9 +1,17 @@
+/*
+时间:2018.05.17
+说明:用于文本的处理，对应的函数均有说明
+注意:尚未对文本的边界条件进行限制，即对异常进行处理
+*/
 #pragma once
 #include <iostream>
 #include<fstream>
 #include<string>
 #include<fstream>
+#include<vector>
+
 using namespace std;
+
 class Joints
 {
 public:
@@ -18,16 +26,29 @@ public:
 	string joint_name;
 	string filename;
 
+	vector<string> _strvec;                           //TXT文件缓存区
+	vector<string> _jointmotiondatas;                 //存放所有关节运动数据
+
 	//读取BVH文件的行数
 	int _linecounts;
-	int _jointline;
-	int _jointnumb;                               //指定关节所在编号
-	int _motionlinenumb;                          //motion 关键字所在行号
+	int _frame;                                       //帧数
 
 	Joints(std::string filename);
-	void read_joint_name_lines(string joint_name);//读取输入的关节所在的行号
-	Joints(std::string filename, std::string joint_name);
+	void read_joint_name_lines(string joint_name);     //读取输入的关节所在的行号
+	//void ResetLine(string filename, int line);       //读取指定行数得内容
+	void read_data_start_lines(string line);           //表明从该行开始读取数据
 
-	
+	Joints(std::string filename, std::string joint_name);
+	double readXroation(string joints);
+	double readYroation(string joints);
+	double readZroation(string joints);
+	void movetonextframe(); //移动帧数
+	//int returnFrame();      //
+
+private:
+	int _jointline;
+	int _jointnumb;                                   //指定关节所在编号
+	int _motiondatalinenumb;                          //motion 关键字所在行号
+	double stringTodouble(string str);
 };
 
